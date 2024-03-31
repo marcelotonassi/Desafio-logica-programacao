d2824643e2b095324bb7025edaa67a29a1121d6b
marcelotonassi <marcelo.tonassi@gmail.com>
2024-03-30 21:13:38 -0300

    Novo commit

--- IndexTest.js
+++ IndexTest.js
@@ -1,27 +1,27 @@
 // Variáveis
-let nomeHeroi = "Superman";
-let xpHeroi = 7500; // Exemplo de experiência
+let nome_Heroi = "SuperMarcelo";
+let xp_Heroi = 7500; // Exemplo de experiência
 
 // Estrutura de decisão
 let nivel;
 
-if (xpHeroi < 1000) {
+if (xp_Heroi < 1000) {
     nivel = "Ferro";
-} else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
+} else if (xp_Heroi >= 1001 && xp_Heroi <= 2000) {
     nivel = "Bronze";
-} else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
+} else if (xp_Heroi >= 2001 && xp_Heroi <= 5000) {
     nivel = "Prata";
-} else if (xpHeroi >= 5001 && xpHeroi <= 7000) {
+} else if (xp_Heroi >= 5001 && xp_Heroi <= 7000) {
     nivel = "Ouro";
-} else if (xpHeroi >= 7001 && xpHeroi <= 8000) {
+} else if (xp_Heroi >= 7001 && xp_Heroi <= 8000) {
     nivel = "Platina";
-} else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
+} else if (xp_Heroi >= 8001 && xp_Heroi <= 9000) {
     nivel = "Ascendente";
-} else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
+} else if (xp_Heroi >= 9001 && xp_Heroi <= 10000) {
     nivel = "Imortal";
 } else {
     nivel = "Radiante";
 }
 
 // Saída
-console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel);
+console.log("O Herói de nome " + nome_Heroi + " está no nível de " + nivel);
