abf9930fb6bafaad27d3e8d2011d23261f2a1c37
marcelotonassi <marcelo.tonassi@gmail.com>
2024-03-30 21:01:59 -0300

    Meu terceiro commit

--- IndexTest.js
+++ IndexTest.js
@@ -1,2 +1,27 @@
-console.log("Marcelo Alves Tonassi")
-console.log("Marcelo Alves Tonassi")
+// Variáveis
+let nomeHeroi = "Superman";
+let xpHeroi = 7500; // Exemplo de experiência
+
+// Estrutura de decisão
+let nivel;
+
+if (xpHeroi < 1000) {
+    nivel = "Ferro";
+} else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
+    nivel = "Bronze";
+} else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
+    nivel = "Prata";
+} else if (xpHeroi >= 5001 && xpHeroi <= 7000) {
+    nivel = "Ouro";
+} else if (xpHeroi >= 7001 && xpHeroi <= 8000) {
+    nivel = "Platina";
+} else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
+    nivel = "Ascendente";
+} else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
+    nivel = "Imortal";
+} else {
+    nivel = "Radiante";
+}
+
+// Saída
+console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel);
